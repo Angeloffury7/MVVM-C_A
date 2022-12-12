58cbc5133ba05a6b148247908d32e3c0c9c3b803
Veronica Rudiuk <veronicarudiuk@gmail.com>
2022-12-12 23:59:15 +0400

    Initial Commit

---
+++ MVVM-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-4
+//
+//  Created by Veronica Rudiuk on 12.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
